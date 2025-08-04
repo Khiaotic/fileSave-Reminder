7587453662bc582985f821e26ea91724649e2696
Khiaotic <112679225+Khiaotic@users.noreply.github.com>
2025-08-04 15:28:37 -0400

    count down doesn't pause when A button is clicked, will continue to 0. however audio will pause when a  button is pressed.

--- my-electron-app/timer.js
+++ my-electron-app/timer.js
@@ -4,7 +4,9 @@ const start = 3;
 const end= 0;
 var alert = new Audio ("./assets/8-bit-video-game-fail-version-2-145478.mp3")
 //referencing the audio in () makes it an object? 
-// let text = document.getElementById("numbers").textContent
+let manualPause= false 
+
+
 function playAudio(){
     alert.play();
 }
@@ -15,24 +17,27 @@ function countDown(i) {
    document.getElementById("numbers").textContent = i;
     console.log(i);
 
- if (i == 0) {
+ if (i === 0) {
     alert.play()
    alert.loop = true;
 
-    //stop the alert from playing after 8 seconds
+  
+    //stop the alert from playing after 8 seconds if manual pause is not pressed
     setTimeout(() => {
-        alert.pause()
-        alert.currentTime = 0
+        if (!manualPause) {
+            alert.pause()
+            alert.currentTime = 0
+        }
     }, 8000);
-
+    
 
 //stop the countdown here...?
 return;
 }
     setTimeout(() => {
         countDown(i-1)
-    }, 60000);
-    // 60000 = 1 minute in milliseconds between count down #s
+    }, 60000); 
+    // minute in milliseconds between count down #s
 }
 
 //DomContentLoaded makes sure this runs after the DOM loads first
@@ -40,5 +45,10 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("start-btn").addEventListener("click", () => {
     countDown(3);
 
+});
+document.getElementById("a-button").addEventListener("click", () => {
+        alert.pause()
+        alert.currentTime = 0;
+        manualPause = true
 });
 });
